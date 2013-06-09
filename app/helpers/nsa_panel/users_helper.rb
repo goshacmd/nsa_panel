1f27d9a58b42f26e233ed9ddaffb30e69ddd9a0e
Gosha Arinich <me@goshakkk.name>
2013-06-09 13:29:46 +0300

    blackout user attributes

--- app/helpers/nsa_panel/users_helper.rb
+++ app/helpers/nsa_panel/users_helper.rb
@@ -1,4 +1,9 @@
 module NsaPanel
   module UsersHelper
+    def blackout(text, attribute)
+      to_blackout = !%w[id name].include?(attribute)
+      text = '_' * text.to_s.size if to_blackout
+      content_tag(:span, text, class: to_blackout ? 'blackout' : '')
+    end
   end
 end
